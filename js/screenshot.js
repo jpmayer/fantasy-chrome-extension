1a12dc052a9b0e26ab3e30709d770ddf07b6478f
Joshua Mayer <joshua@Joshuas-iMac.local>
2018-08-25 20:22:25 -0700

    Remove second script call of screenshot, change to tab instead of window

--- js/screenshot.js
+++ js/screenshot.js
@@ -1,23 +1,33 @@
 'use strict';
 
 const saveScreenshot = (callback) => {
+  console.log("adding listener");
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+      console.log("listener", request);
       if (request.msg === "something_completed") {
         document.getElementById("create-screenshot").innerHTML = request.data.html;
-        html2canvas(document.getElementById("create-screenshot"), 
+        html2canvas(document.getElementById("create-screenshot"),
           {width: "fit-content", allowTaint: true}).then(function(canvas) {
             document.getElementById("screenshot-img").src = canvas.toDataURL();
             canvas.toBlob(function(blob) {
-              saveAs(blob, request.data.name + ".png"); 
+              saveAs(blob, request.data.name + ".png");
+              callback();
             });
             //Comment the below line to retain the original table
             document.getElementById("create-screenshot").style.display = "none";
-            callback(true)
         });
+      } else {
+        console.log(request);
       }
     }
   );
 }
 
-saveScreenshot(function(){ /*window.close();*/ });
+const closeWindow = () => {
+  setTimeout(() => {
+    window.close();
+  }, 1000);
+}
+
+saveScreenshot(closeWindow);
